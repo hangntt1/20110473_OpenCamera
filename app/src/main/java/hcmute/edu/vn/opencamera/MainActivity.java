6055538ff98368939ff7aad407f8bd1ace8e8670
hangntt1 <hangn3569@gmail.com>
2023-03-27 13:32:12 +0700

    first commit

---
+++ app/src/main/java/hcmute/edu/vn/opencamera/MainActivity.java
@@ -0,0 +1,48 @@
+package hcmute.edu.vn.opencamera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Define the pic id
+    private static final int pic_id = 123;
+    // Define the button and imageview type variable
+    Button camera_open_id;
+    ImageView click_image_id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // By ID we can get each component which id is assigned in XML file get Buttons and imageview.
+        camera_open_id = findViewById(R.id.camera_button);
+        click_image_id = findViewById(R.id.click_image);
+
+        camera_open_id.setOnClickListener(v -> {
+
+            Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            startActivityForResult(camera_intent, pic_id);
+        });
+    }
+
+    // This method will help to retrieve the image
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Match the request 'pic id with requestCode
+        if (requestCode == pic_id) {
+            // BitMap is data structure of image file which store the image in memory
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            // Set the image in imageview for display
+            click_image_id.setImageBitmap(photo);
+        }
+    }
+}
